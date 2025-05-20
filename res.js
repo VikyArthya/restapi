aa654380c256f2f0d781743d8783ec617276f471
VikyArthya <vikyarthya1945@gmail.com>
2025-05-20 22:52:55 +0700

    json nested

--- res.js
+++ res.js
@@ -1,12 +1,35 @@
-'use strict' //biar ketat kodenya tidak salah
+"use strict"; //biar ketat kodenya tidak salah
 
-exports.ok = function(values,res){
-    var data = {
-        'status':200,
-        'values':values
-    };
+exports.ok = function (values, res) {
+  var data = {
+    status: 200,
+    values: values,
+  };
 
-    res.json(data);
-    res.end();
-}
+  res.json(data);
+  res.end();
+};
 
+exports.oknested = (values, res) => {
+  const hasil = values.reduce((akumulasikan, item) => {
+    if (akumulasikan[item.nama]) {
+      const group = akumulasikan[item.nama];
+      if (Array.isArray(group.matakuliah)) {
+        group.matakuliah.push(item.matakuliah);
+      } else {
+        group.matakuliah = [group.matakuliah, item.matakuliah];
+      }
+    } else {
+      akumulasikan[item.nama] = item;
+    }
+    return akumulasikan;
+  }, {});
+
+  var data = {
+    status: 200,
+    values: hasil,
+  };
+
+  res.json(data);
+  res.end();
+};
