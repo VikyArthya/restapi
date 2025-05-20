3ba6a5bc70e5524cfb0758afc6f021873eccf39b
VikyArthya <vikyarthya1945@gmail.com>
2025-05-21 01:19:48 +0700

    resgiter api

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var morgan = require("morgan");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
-var routes = require('./routes')
-routes(app)
+var routes = require("./routes");
+routes(app);
+
+app.use("/auth", require("./middleware"));
 
 app.listen(3000, () => {
-    console.log(`Server started on port http://localhost:3000`);
+  console.log(`Server started on port http://localhost:3000`);
 });
-
